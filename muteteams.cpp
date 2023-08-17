7f54ee2dfdfaa53475c5f984af1ddbd850579fa6
kirin123 <kirin123kirin@gmail.com>
2023-08-17 11:25:19 +0900

    v0.3.0

--- muteteams.cpp
+++ muteteams.cpp
@@ -3,12 +3,15 @@
 
 #define BUFSIZE 512
 
+/* 会議中のウィンドウ名検索ワード */
 const wchar_t* meating_chk = L" | Microsoft Teams";
 const wchar_t call_chk[] = L"Microsoft Teams 通話中";
 
-void Send_KeyMap(int keymap[], unsigned int len) {
+/* キーボードを押下する信号を送信する */
+void Send_Keys(int* keymap, unsigned int len) {
     INPUT inputs[len];
 
+    /* キーを押下する */
     for(unsigned int i = 0; i < len; i++) {
         inputs[i].type = INPUT_KEYBOARD;
         inputs[i].ki.wVk = keymap[i];
@@ -16,29 +19,39 @@ void Send_KeyMap(int keymap[], unsigned int len) {
         SendInput(1, &inputs[i], sizeof(INPUT));
     }
 
+    /* 押下したキーを上げる */
     for(unsigned int i = 0; i < len; i++) {
         inputs[i].ki.dwFlags = KEYEVENTF_KEYUP;
         SendInput(1, &inputs[i], sizeof(INPUT));
     }
 }
 
-BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lpr) {
+/* 会議中のTeamsウィンドウタイトルを探しミュートコマンド発行する */
+BOOL CALLBACK TeamsGlobalMute(HWND hwnd, LPARAM lpr) {
     wchar_t B[BUFSIZE] = {0};
+
+    /* Windowタイトルを探す */
     if(GetWindowTextW(hwnd, B, BUFSIZE)) {
+        /* 通話中のウィンドウがあったらウィンドウフォーカスを当てる */
         if(memcmp(B, call_chk, sizeof(call_chk) - sizeof(call_chk[0])) == 0)
             ShowWindow(hwnd, SW_RESTORE);
 
+        /* 会議中のウィンドウを探す */
         if(wcsstr(B, meating_chk)) {
+            /* 会議中のウィンドウが最小化されてたら元のサイズに戻す */
             if(IsIconic(hwnd))
                 ShowWindow(hwnd, SW_RESTORE);
 
+            /* 会議中のウィンドウを最前面に出す */
             SetForegroundWindow(hwnd);
+
+            /* Teamsのミュートコマンド Ctrl+Shift+Mのキーマップを発行する */
             int kmap[] = {
                 VK_CONTROL,  // Ctrl
                 VK_SHIFT,    // Shift
                 0x4D,        // ASCII M
             };
-            Send_KeyMap(kmap, sizeof(kmap) / sizeof(kmap[0]));
+            Send_Keys(kmap, sizeof(kmap) / sizeof(kmap[0]));
             return FALSE;
         }
     }
@@ -48,18 +61,38 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lpr) {
 int MuteHandler() {
     INPUT input;
     input.type = INPUT_KEYBOARD;
+
+    int arry[256] = {0};
+    int* p = arry;
+
+    /* キー押下されてたら強制的に離す処理 */
     input.ki.dwFlags = KEYEVENTF_KEYUP;
     for(int code = 0; code < 256; ++code) {
         if(GetAsyncKeyState(code) & 0x8000) {
+            *p++ = code;
             input.ki.wVk = code;
             if(SendInput(1, &input, sizeof(INPUT)) == 0)
-                return 1;
+                break;
         }
     }
-    return EnumWindows(EnumWindowsProc, NULL) ? 0 : 1;
+
+    /* 処理本体 */
+    int ret = EnumWindows(TeamsGlobalMute, NULL) ? 0 : 1;
+
+    /* 処理開始前に押下されていたキーを押されてた状態に復元する */
+    input.ki.dwFlags = 0;
+    p = arry;
+    for(; *p; ++p) {
+        input.ki.wVk = *p;
+        if(SendInput(1, &input, sizeof(INPUT)) == 0)
+            break;
+    }
+    return ret;
 }
 
-/* https://stackoverflow.com/questions/48915216/link-error-when-compiling-win32-application-with-clion-cmake-msvc-2015
+/*
+    コマンドプロンプトウィンドウを表示させないためのおまじない
+    https://stackoverflow.com/questions/48915216/link-error-when-compiling-win32-application-with-clion-cmake-msvc-2015
  */
 int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     // int main() {
