b0435fe548a1470116d7bcafd73cc5a454e03fae
kirin123 <kirin123kirin@gmail.com>
2023-08-17 09:17:18 +0900

    v0.2.2

--- muteteams.cpp
+++ muteteams.cpp
@@ -6,18 +6,12 @@
 const wchar_t* meating_chk = L" | Microsoft Teams";
 const wchar_t call_chk[] = L"Microsoft Teams 通話中";
 
-void Send_Ctrl_Shift_M() {
-    int val[] = {
-        VK_CONTROL,  // Ctrl
-        VK_SHIFT,    // Shift
-        0x4D,        // ASCII M
-    };
-    const unsigned int len = sizeof(val) / sizeof(val[0]);
+void Send_KeyMap(int keymap[], unsigned int len) {
     INPUT inputs[len];
 
     for(unsigned int i = 0; i < len; i++) {
         inputs[i].type = INPUT_KEYBOARD;
-        inputs[i].ki.wVk = val[i];
+        inputs[i].ki.wVk = keymap[i];
         inputs[i].ki.dwFlags = 0;
         SendInput(1, &inputs[i], sizeof(INPUT));
     }
@@ -39,33 +33,35 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lpr) {
                 ShowWindow(hwnd, SW_RESTORE);
 
             SetForegroundWindow(hwnd);
-            Send_Ctrl_Shift_M();
-            exit(0);
+            int kmap[] = {
+                VK_CONTROL,  // Ctrl
+                VK_SHIFT,    // Shift
+                0x4D,        // ASCII M
+            };
+            Send_KeyMap(kmap, sizeof(kmap) / sizeof(kmap[0]));
+            return FALSE;
         }
     }
     return TRUE;
 }
 
-bool AnyKeyPressed() {
+int MuteHandler() {
+    INPUT input;
+    input.type = INPUT_KEYBOARD;
+    input.ki.dwFlags = KEYEVENTF_KEYUP;
     for(int code = 0; code < 256; ++code) {
-        if(GetAsyncKeyState(code) & 0x8000)
-            return true;
+        if(GetAsyncKeyState(code) & 0x8000) {
+            input.ki.wVk = code;
+            if(SendInput(1, &input, sizeof(INPUT)) == 0)
+                return 1;
+        }
     }
-    return false;
+    return EnumWindows(EnumWindowsProc, NULL) ? 0 : 1;
 }
 
 /* https://stackoverflow.com/questions/48915216/link-error-when-compiling-win32-application-with-clion-cmake-msvc-2015
  */
 int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     // int main() {
-
-    try {
-        int timeout = 100;
-        while(AnyKeyPressed() && --timeout)
-            Sleep(10);
-
-        EnumWindows(EnumWindowsProc, NULL);
-    } catch(...) {
-    }
-    return 0;
+    return MuteHandler();
 }
